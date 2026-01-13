af3c442ece61e055161cd666620848fa2de2deec
Lier0102 <minehammer26@gmail.com>
2026-01-13 17:32:33 +0900

    docker is killing me

--- pwn/dreamhack_/iofile_vtable_check/ex.py
+++ pwn/dreamhack_/iofile_vtable_check/ex.py
@@ -9,6 +9,10 @@ from pwn import *
 
 이론...인데 수정하는 게 어려워 보인다.
 도커 구성 하려 했지만 18.04라 크게 다른 건 없다고 생각해서 지금은 일단 이론 찾아보면서 만드는중
+
+마지막에 fclose() 있으니까
+JUMP_FIELD(_IO_finish_t, __finish); // fclose()
+이거 쓰면 되지 않나?
 '''
 
 context.binary = elf = ELF("./iofile_vtable_check")
