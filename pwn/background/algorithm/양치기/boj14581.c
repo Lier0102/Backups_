5803e1f811fbf7af8b3efff6635ff8950be60bcb
Lier0102 <minehammer26@gmail.com>
2026-02-08 21:32:34 +0900

    20문제 완

---
+++ pwn/background/algorithm/양치기/boj14581.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char a[21];
+    scanf("%s", a);
+
+    puts(":fan::fan::fan:");
+    printf(":fan::%s::fan:\n", a);
+    printf(":fan::fan::fan:");
+
+    return 0;
+}
